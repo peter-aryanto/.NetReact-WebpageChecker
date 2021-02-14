f43a03dc46c9f5ec20291e72d4fec3479072d498
Peter Aryanto <peter.aryanto@gmail.com>
2021-02-14 22:08:00 +1100

    Added WebpageChecker component mimicking FetchData (WeatherForecast) component.

--- WebpageChecker/ClientApp/src/components/Home.tsx
+++ WebpageChecker/ClientApp/src/components/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => (
     </ul>
     <p>To help you get started, we've also set up:</p>
     <ul>
-      <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
+      <li><strong>Client-side navigation</strong>. For example, click either <em>Counter</em> or <em>Webpage Checker</em> then <em>Back</em> to return here.</li>
       <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
       <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
     </ul>
--- WebpageChecker/ClientApp/src/components/NavMenu.tsx
+++ WebpageChecker/ClientApp/src/components/NavMenu.tsx
@@ -13,7 +13,7 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }
             <header>
                 <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom box-shadow mb-3" light>
                     <Container>
-                        <NavbarBrand tag={Link} to="/">WebpageChecker</NavbarBrand>
+                        <NavbarBrand tag={Link} to="/">Webpage Checker</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} className="mr-2"/>
                         <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={this.state.isOpen} navbar>
                             <ul className="navbar-nav flex-grow">
@@ -25,6 +25,9 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }
                                 </NavItem>
                                 <NavItem>
                                     <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/WebpageChecker">Webpage Checker</NavLink>
                                 </NavItem>
                             </ul>
                         </Collapse>
